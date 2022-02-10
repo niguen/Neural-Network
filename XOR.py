67bc65723f27756d7671f6c48872fb8ddb1eb246
Günther, Niklas (mdniguen) <niklas.guenther@stud.hs-ruhrwest.de>
2022-02-10 15:07:19 +0100

    Upload New File

---
+++ XOR.py
@@ -0,0 +1,29 @@
+from neuralNetwork_matrix import neuralNetwork
+
+network = neuralNetwork(2, 10, 1, 0.2)
+
+inputs = [[0, 0], [0, 1], [1, 0], [1, 1]]
+outputs = [[0], [1], [1], [0]]
+
+print("Expected result:" + str(outputs[0]))
+print(network.query(inputs[0]))
+
+# network.train(inputs[0], outputs[0])
+
+for i in range(10000):
+    for record in range(len(inputs)):
+        network.train(inputs[record], outputs[record])
+
+print("Expected result:" + str(outputs[0]))
+print(network.query(inputs[0]))
+
+print("Expected result:" + str(outputs[1]))
+print(network.query(inputs[1]))
+
+print("Expected result:" + str(outputs[2]))
+print(network.query(inputs[2]))
+
+print("Expected result:" + str(outputs[3]))
+print(network.query(inputs[3]))
+
+
