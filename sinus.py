4e479bc32518af2e1a6eaa3ce6c5e1c04880b412
Günther, Niklas (mdniguen) <niklas.guenther@stud.hs-ruhrwest.de>
2022-02-12 18:48:02 +0100

    Update sinus.py

--- sinus.py
+++ sinus.py
@@ -1,34 +1,45 @@
+import random
 from math import sin
 
-import matplotlib.pyplot as plt
-
-from neuralNetwork_matrix import neuralNetwork
 from matplotlib import pyplot as plt
 
-network = neuralNetwork(1, 20, 1, 0.5)
+from neuralNetwork import neuralNetwork
+
+network = neuralNetwork(1, 10, 1, 0.4)
 
 values = []
-numValues = 10
+numValues = 7000
 
 step = (2 * 3.1415) / numValues
 x = 0
 for i in range(numValues):
-    values.append(x)
+    values.append(x / (2 * 3.1415))
     x += step
+random.shuffle(values)
+print(values)
 
-for x in range(10000):
-    for i in range(numValues):
-        current = values[i]
-        network.train(current, sin(current))
+
+targets = []
+for i in range(numValues):
+    targets.append((0.5 * sin(values[i] * (2 * 3.1415))) + 0.5)
+
+trainData = values[:6000]
+trainTargets = targets[:6000]
+
+testData = values[6000:]
+testTargets = values[6000:]
+
+
+for x in range(1000):
+    for i in range(6000):
+        network.train(trainData[i], trainTargets[i])
     print(str(x) + "%")
 
 results = []
-expected = []
-for i in range(numValues):
-    current = network.query(values[i])
-    results.append(current.values[0])
-    expected.append(sin(values[i]))
+for i in range(1000):
+    current = network.query(testData[i])
+    results.append(current[0])
 
-plt.plot(values, expected)
-plt.plot(values, results)
+plt.plot(values, targets, 'x')
+plt.plot(testData, results, 'o')
 plt.show()
